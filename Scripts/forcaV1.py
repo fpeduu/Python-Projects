ea580a100db0d1110db940e3a80506413dfb8d75
Filipe Eduardo <filipeed14@gmail.com>
2020-03-18 15:28:43 -0300

    Pasta de Scripts

--- Scripts/forcaV1.py
+++ Scripts/forcaV1.py
@@ -1,47 +1,74 @@
+#Tarefas p/ próximo encontro.
+#Melhorar o código
+#Mostrar funcionamento
+#Explicar funcionamento
+#Dividir tarefas em funções
+
 from random import randint
+
+print('Jogo da forca')
+
 def jogar():
-    print('Jogo da forca')
-    palavras = ['banana','comida','escola','computador','trabalho','estudar']
-    pal_sec = palavras[randint(0,5)]
-    let_ace = str
-    acertou = False
+    palavras = ['recife','programar','bonito','banana','comida','escola','computador','trabalho','estudar']
+    pal_sec = palavras[randint(0, len(palavras)-1)]
     erros = 0
-    enforcado = False
-    if pal_sec == 'banana' or pal_sec == 'comida' or pal_sec == 'escola':
-       let_ace = ["-","-","-","-","-","-"]
-    elif pal_sec == 'computador':
-        let_ace = ["-","-","-","-","-","-","-","-","-","-"]
-    elif pal_sec == 'trabalho':
-        let_ace = ["-","-","-","-","-","-","-","-"]
-    else:
-        let_ace = ["-","-","-","-","-","-","-"]
-    while not enforcado and not acertou:
-        print("".join(let_ace))
-        chute = str(input("Digite uma letra. "))
+    let_ace = []
+    for c in pal_sec:
+        let_ace += '-'
+    fim = False
+
+
+    def verificaChute(erros):
         if chute in pal_sec:
+            print("Acertou.")
             pos = 0
-            count = 0
             for letra in pal_sec:
                 if chute.upper() == letra.upper():
                     let_ace[pos] = chute.lower()
-                    count += 1
-                
                 pos+=1
-        if count == 1:
-            print("Tem 1 letra {} na palavra secreta. ".format(chute.upper()))
-        if count > 1:
-            print("Tem {} letras {} na palavra secreta. ".format(count,chute.upper()))
-        if not chute in pal_sec:
+        else:
             erros += 1
-            print("Não tem nenhuma letra {} na palavra secreta. ".format(chute.upper()))
+            print("Errou.".format(chute.upper()))
             print("Erros = {}".format(erros))
+        return(erros)    
+
+    def venceuOuPerdeu(erros, fim):
         if not "-" in let_ace:
             print("".join(let_ace))
-            acertou = True
-        if erros >= 3:
-            enforcado = True
-    if acertou == True:
-       print("Parabéns, você acertou a palavra secreta. \n{}. ".format("".join(let_ace)))
-    if enforcado == True:
-        print("Eita. Você perdeu. \nA palavra secreta era {}. ".format("".join(pal_sec)))
-    print("""Digite "jogar()" para jogar novamente. """)
+            print("Parabéns, você acertou a palavra secreta. \n{}. ".format(''.join(let_ace)))
+            fim = True
+        elif erros >= 3:
+            print("Eita. Você perdeu. \nA palavra secreta era {}. ".format(pal_sec))
+            fim = True
+        return fim
+
+
+    while fim == False:
+        print(''.join(let_ace))
+        chute = str(input("Digite uma letra. "))
+        
+        erros = verificaChute(erros)
+
+        fim = venceuOuPerdeu(erros, fim)
+
+
+jogar()
+
+
+repetir = True
+
+def continuar(repetir):
+    continuar = input('Deseja continuar? [S/N]')
+    if continuar.upper() == 'S':
+        jogar()
+    elif continuar.upper() == 'N':
+        print('Até a próxima. ')
+        repetir = False
+    else:
+        print('Opção inválida')
+        repetir = True
+    return repetir
+
+
+while repetir:
+    repetir = continuar(repetir)
